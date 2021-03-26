08797a23984f7ae0054699a23efec55abcaab053
a.dikii <andrey.dikiy@x5.ru>
2021-03-26 14:39:44 +0300

    initial commit

---
+++ src/main/java/ru/tender/pricing/PricingApplication.java
@@ -0,0 +1,18 @@
+package ru.tender.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import ru.tender.pricing.util.ApplicationUtil;
+
+@SpringBootApplication
+public class PricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(PricingApplication.class);
+		ApplicationUtil.addDefaultProfile(app);
+		Environment environment = app.run(args).getEnvironment();
+		ApplicationUtil.logApplicationStartup(environment);
+	}
+
+}
---
+++ src/main/java/ru/tender/pricing/util/ApplicationUtil.java
@@ -0,0 +1,59 @@
+package ru.tender.pricing.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ApplicationUtil {
+
+    private static final String SPRING_PROFILE_DEFAULT = "spring.profiles.default";
+    private static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+
+    public static void addDefaultProfile(SpringApplication app) {
+        Map<String, Object> defProperties = new HashMap<>();
+        defProperties.put(SPRING_PROFILE_DEFAULT, SPRING_PROFILE_DEVELOPMENT);
+        app.setDefaultProperties(defProperties);
+    }
+
+    public static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
+    }
+}
