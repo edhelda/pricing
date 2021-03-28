c9dcb4636fb9aebb2a53a18e6d4b21f38d5917c2
andrey.dikiy <andrey.dikiy@x5.ru>
2021-03-28 12:25:00 +0300

    Добавлен swagger

--- src/main/java/ru/tender/pricing/controller/PricingController.java
+++ src/main/java/ru/tender/pricing/controller/PricingController.java
@@ -1,9 +1,12 @@
 package ru.tender.pricing.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +15,12 @@ import ru.tender.pricing.service.PricingService;
 import ru.tender.pricing.service.dto.Filter;
 import ru.tender.pricing.service.dto.PricingDTO;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@Validated
 @RestController
+@Api(tags = "Продукты")
 @RequestMapping("/api/pricing")
 @RequiredArgsConstructor
 public class PricingController {
@@ -22,7 +28,8 @@ public class PricingController {
     private final PricingService pricingService;
 
     @PostMapping("/find-all")
-    public ResponseEntity<Page<PricingDTO>> findAll(@RequestBody List<Filter> filters, Pageable pageable) {
+    @ApiOperation("Поиск продуктов")
+    public ResponseEntity<Page<PricingDTO>> findAll(@RequestBody @Valid List<Filter> filters, Pageable pageable) {
         Page<PricingDTO> resultPage = pricingService.findAll(filters, pageable);
         return ResponseEntity.ok(resultPage);
     }
